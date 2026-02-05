57a3c1bb393e9377e79057abd232e118d7b9b3b0
angelaysolis <angela.y.solis-sarat@wmich.edu>
2026-02-05 16:27:05 -0500

    Add files via upload

---
+++ learning_bash.sh
@@ -0,0 +1,11 @@
+#!/bin/bash/learning_bash.sh
+name=Angela 
+greeting=Hola
+echo "$greeting $name"
+echo "This script is helping you learn bash"
+mkdir ~/Desktop/bioinformatics
+cd ~/Desktop/bioinformatics
+mkdir ~/Desktop/bioinformatics/bash_scripts
+cd ~/Desktop/bioinformatics/bash_scripts
+mv ~/Desktop/bash_template.txt ~/Desktop/bioinformatics/bash_scripts/
+mv bash_template.txt bash_template.sh
